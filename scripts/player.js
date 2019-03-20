9b313fbad5ccbd06129464b47b6bc6b817bfb078
Sydney Bouchard <sydneykbouchard@gmail.com>
2019-03-20 17:52:00 -0400

    fix volume

--- scripts/player.js
+++ scripts/player.js
@@ -38,6 +38,22 @@ class Player {
     }
   }
 
+  prettyTime(time) {
+    var seconds = Number.parseFloat(time);
+    var wholeSeconds = Math.floor(seconds);
+    var minutes = Math.floor(wholeSeconds / 60);
+
+    var remainingSeconds = wholeSeconds % 60;
+    var output = minutes + ':';
+
+    if (remainingSeconds < 10) {
+      output += '0';
+    }
+
+    output += remainingSeconds;
+    return output;
+  }
+
   skipTo (percent) {
     if (this.playState !== 'playing') { return }
     this.soundObject.setTime( (percent / 100) * this.soundObject.getDuration() );
